f042130631489bb995df1bda374097b3805f752c
Andrew Zamojc <andrewzamojc@gmail.com>
2015-10-23 15:43:39 -0400

    implemented POC facebook-customer login endpoint

--- index.js
+++ index.js
@@ -1,33 +1,151 @@
 var express     = require('express');
 var app         = express();
 var shopifyAPI  = require('shopify-node-api');
+var $           = require('jQuery');
+var request     = require('request');
+var url         = require('url');
+var fb          = require('fb');
 
+// temp global to test changing the password
+var newPassword = 'qwerty10';
+
+var Shopify = new shopifyAPI({
+    shop: 'arbor-natural',
+    // use .env file for env vars with local heroku
+    shopify_api_key: process.env.SHOPIFY_API_KEY,
+    access_token: process.env.SHOPIFY_API_PASSWORD
+});
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
+app.get('/signin-with-facebook', function(request, response) {
     console.log("Request handler at root was called.");
 
-    // API KEYS AND SECRET IN CODE
-    // POTENTIAL SECURITY RISK WHEN STORE IS LIVE?
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
 
-    var Shopify = new shopifyAPI({
-            shop: 'arbor-natural', // MYSHOP.myshopify.com
-            shopify_api_key: '3b207321ad5149095622b128806166cc', // Your API key
-            access_token: 'b56d7404b510820527ef520c1958a5ae' // Your API Password
+    var accessToken = query.access_token;
+    fb.setAccessToken(accessToken);
+
+    console.log('Welcome!  Fetching your information.... ');
+    fb.api('/me', function(fbResponse) {
+        console.log('WOOO======================================');
+        console.log(fbResponse);
+        console.log('Successful login for: ' + fbResponse.name);
+
+        findCustomer('1742644675', function (data1) {
+            var html = data1.body;
+
+            console.log('CUSTOMER  DATA');
+            console.log(data1);
+
+            resetCustomerPassword('1742644675', function (data2) {
+                console.log('PW RESET  DATA');
+                console.log(data2);
+
+                // createCustomer('newfirst1', 'newlast1', 'newfirst1@gmail.com', 'freefree', function(data) {
+                //     response.writeHead(200, {"Content-Type": "application/json"});
+                //     response.end(data);
+                // });
+
+                login('newfirst1@gmail.com', newPassword, function (data3) {
+                    var html = data3.body;
+
+                    console.log('data333333333333333333================================');
+                    console.log(data3);
+                    console.log('data333333333333333333================================');
+                    console.log('data333333333333333333================================');
+                    var loc = JSON.stringify(data3.response.headers.location);
+                    console.log(loc);
+
+
+
+                    if (html.indexOf('Invalid login credentials') !== -1) {
+                        // invalid password - show a message to the user
+                        console.log('INVALID PASSWORD');
+                        response.writeHead(200, {"Content-Type": "application/json"});
+                        response.end('IT DIDNT');
+                    } else {
+                        console.log('REDIRECTING AFTER LOGIN');
+                        // var checkoutUrl = getCheckoutUrl();
+                        // if (checkoutUrl) {
+                        //     window.location.href = checkoutUrl;
+                        // } else {
+                        //     console.log('ERROR NO REDIRECT URL')
+                        // }
+                        response.writeHead(302, {
+                            'Location': data3.response.headers.location
+                            //add other headers here...
+                        });
+                        response.end();
+                    }
+                });
+            });
+        });
+    });
+
+    console.log('DONE GET FUNCTION');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+function isFunction(functionToCheck) {
+    var getType = {};
+    return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
+}
+
+function findCustomer(id, done) {
+    Shopify.get('/admin/customers/' + id + '.json', function(err, data, headers) {
+        // console.log(err);
+        // console.log(data);
+        // console.log(headers);
+
+        var json = JSON.stringify({
+            err: err,
+            data: data,
+            headers: headers
         });
 
+        if (isFunction(done)) {
+            done(json);
+        }
+    });
+}
+
+function resetCustomerPassword(id, done) {
+    Shopify.put('/admin/customers/' + id + '.json', {
+        "customer": {
+            "password": newPassword,
+            "password_confirmation": newPassword
+        }
+    }, function(err, data, headers) {
+        // console.log(err);
+        // console.log(data);
+        // console.log(headers);
+
+        var json = JSON.stringify({
+            err: err,
+            data: data,
+            headers: headers
+        });
+
+        if (isFunction(done)) {
+            done(json);
+        }
+    })
+}
+
+function createCustomer(firstName, lastName, email, password, done) {
     var postData = {
         "customer": {
-            "first_name": "first11",
-            "last_name": "last11",
-            "email": "firstlast11@gmail.com"
+            "first_name": firstName,
+            "last_name": lastName,
+            "email": email,
+            "password": password,
+            "password_confirmation": password,
+            "send_email_welcome": false
         }
     };
 
@@ -42,14 +160,47 @@ app.get('/', function(request, response) {
             headers: headers
         });
 
-        response.writeHead(200, {"Content-Type": "application/json"});
-        response.end(json);
+        if (isFunction(done)) {
+            done(json);
+        }
     });
+}
 
-    console.log('DONE');
-});
+function login(email, password, done) {
+    var data = {
+        'customer[email]': email,
+        'customer[password]': password,
+        form_type: 'customer_login',
+        utf8: '✓'
+    };
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+    request({
+        url: 'http://arbor-natural.myshopify.com/account/login', //URL to hit
+        qs: '', //Query string data
+        method: 'POST',
+        //Lets post the following key/values as form
+        form: data
+    }, function(error, response, body){
+        if (isFunction(done)) {
+            done({
+                error: error,
+                response: response,
+                body: body
+            });
+        }
+    });
+}
+
+function getCheckoutUrl() {
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+
+        var regex   = new RegExp("[\\?&]" + name + "=([^&#]*)");
+        var results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+    return getParameterByName('checkout_url');
+}
+
+
 
